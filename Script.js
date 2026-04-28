9880606fc9958f96d8b1fdc738940f5311ae96dd
diegoromero5332-spec <diegoromero5332@gmail.com>
2026-04-27 23:47:57 -0600

    Add files via upload
    mis trabajos

---
+++ Script.js
@@ -0,0 +1,16 @@
+Script.js
+function mostrarmensaje(){
+    alert("ya quedo descargada la playsit");
+}
+function mostrarmensaje(){
+    alert("piano");
+}
+function mostrarmensaje(){
+    alert("rimas");
+}
+function mostrarmensaje(){
+    alert("entonacion");
+}
+function mostrarmensaje(){
+    alert("vestuario");
+}
